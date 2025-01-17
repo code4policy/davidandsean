64b4b8b4bd697bfbe14281cd67396e3027d3c951
David Shi <davidshi@dhcp-10-250-49-95.harvard.edu>
2025-01-17 17:55:05 -0500

    finished the readwrite assignment

---
+++ readwrite.py
@@ -0,0 +1,11 @@
+with open('name.txt') as f:
+    my_name = f.read()
+
+# print output text
+print(my_name)
+
+with open('output/hello.txt', 'w') as f:
+  f.write('Hello, my name is ')
+  f.write(my_name)
+
+
